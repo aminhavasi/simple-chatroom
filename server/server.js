7efb64b76db811d4bc67c60e0349b901ba947a5e
amin havasi <pau.ahq@gmail.com>
2020-03-18 09:34:36 +0330

    config

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log(__dirname);
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname + '/../public');
+app.use(express.static(publicPath));
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
