d0718c4cb6959851075d8f2bc7ec8f117e36037f
amin havasi <pau.ahq@gmail.com>
2020-03-19 03:00:36 +0330

    basic option

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
-const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname + '/../public');
-const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
 const server = http.createServer(app);
+//************************************************** */
 
+const publicPath = path.join(__dirname + '/../public');
 require('dotenv').config();
 app.use(express.static(publicPath));
+const io = socketIo(server);
 
+io.on('connection', socket => {
+    console.log('a new user just connected');
+});
 server.listen(process.env.PORT, () => {
     console.log('server is running on port 3000');
 });
