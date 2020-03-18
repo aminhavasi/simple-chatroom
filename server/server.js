1239087b18b0779d6e5f78743f33d0d5f9ea1ad6
amin havasi <pau.ahq@gmail.com>
2020-03-19 03:12:08 +0330

    debug

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ const io = socketIo(server);
 
 io.on('connection', socket => {
     console.log('a new user just connected');
+    socket.on('disconnect', () => {
+        console.log('disconnect');
+    });
 });
 server.listen(process.env.PORT, () => {
     console.log('server is running on port 3000');
