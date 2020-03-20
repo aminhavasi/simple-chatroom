91c4bc44edcfb02fc27d75e1dd2066f547e76674
amin havasi <pau.ahq@gmail.com>
2020-03-20 14:24:18 +0330

    login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { User, validate } = require('./../models/user');
+const joi = require('@hapi/joi');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 router.post('/register', async (req, res) => {
     try {
@@ -16,4 +18,32 @@ router.post('/register', async (req, res) => {
         res.status(400).send(err);
     }
 });
+router.post('/login', async (req, res) => {
+    const { error } = await loginValidator(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) return res.status(400).send('email or password is wrong');
+
+    const result = await bcrypt.compare(req.body.password, user.password);
+    if (!result) return res.status(400).send('email or password is wrong');
+    user.genToken();
+});
+
+const loginValidator = user => {
+    const schema = joi.object({
+        email: joi
+            .string()
+            .email()
+            .required(),
+        password: joi
+            .string()
+            .min(8)
+            .max(1024)
+            .required()
+    });
+    return schema.validate(user);
+};
 module.exports = router;
